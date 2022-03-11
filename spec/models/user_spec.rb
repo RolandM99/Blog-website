ca752a2392216a3d32385ddae9d097d71d1a07ac
rolandm99 <manfulmweze99@gmail.com>
2022-03-11 17:58:57 +0100

    Add unit specs for all models' methods and validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validates' do
+    subject { User.new(name: 'Tom', photo: 'Tom.png', bio: 'Teacher from Mexico.') }
+
+    before { subject.save }
+
+    it 'should have a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    # it 'return the three latest posts for a user' do
+    #   expect(User).to eql 3
+    # end
+
+    it 'return all the posts for a user' do
+      expect(User.count).to eql 0
+    end
+  end
 end
