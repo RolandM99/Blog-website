2218960481ea40e2db149467073c5b2df604a516
rolandm99 <manfulmweze99@gmail.com>
2022-03-19 11:41:27 +0100

    Add authentification

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,16 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  
+  namespace :api, default: {format: :json} do 
+    namespace :v1,  default: {format: :json} do
+      resources :users, only: [:index, :show] do
+      resources :posts, only: [:index, :show, :create] do
+        resources :comments, only: [:index, :create]
+      end
+    end
+  end
+end
 
   resources :users, only: %i[index show] do 
     resources :posts, only: %i[index new create show destroy]
