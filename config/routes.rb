a070466d96326cf66ea85320d5d89e9c8ecc99ca
rolandm99 <manfulmweze99@gmail.com>
2022-03-19 16:52:03 +0100

    Add changes from review

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,21 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  
-  namespace :api, default: {format: :json} do 
-    namespace :v1,  default: {format: :json} do
-      resources :users, only: [:index, :show] do
-      resources :posts, only: [:index, :show, :create] do
-        resources :comments, only: [:index, :create]
-      end
-    end
-  end
-end
 
-  resources :users, only: %i[index show] do 
+  namespace :api, default: {format: :json} do
+    namespace :v1,  default: {format: :json} do
+       post 'login', to: 'authentication#authenticate'
+       post 'register', to: 'users#create'
+       resources :users do
+         resources :posts, only: [:index, :show, :create] do
+           resources :comments, only: [:index, :create]
+           resources :likes, only: [:create]
+         end
+       end
+     end
+  end
+
+  resources :users, only: %i[index show] do
     resources :posts, only: %i[index new create show destroy]
   end
 
